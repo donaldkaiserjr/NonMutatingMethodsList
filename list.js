915c7f0644d42f204f9eb38288f0d1131912d830
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-11-24 16:08:56 -0800

    Update list.js

--- list.js
+++ list.js
@@ -11,4 +11,18 @@ const arr2 = [...arr1, 'f']; // ['a', 'b', 'c', 'd', 'e', 'f']
 const arr3 = ['z', ...arr1]; // ['z', 'a', 'b', 'c', 'd', 'e']
 
 
+// REMOVE
+1) filter()
+const arr1 = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = arr1.filter(a => a !== 'e'); // ['a', 'b', 'c', 'd']
+// OR
+const arr2 = arr1.filter(a => {
+  return a !== 'e';
+}); // ['a', 'b', 'c', 'd']
+
+
+
+
+
+
 
