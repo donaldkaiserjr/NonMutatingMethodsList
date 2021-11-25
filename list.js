d0a0272da5b235ff3c840999afae9bbd47980c75
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-11-24 16:08:39 -0800

    Update list.js

--- list.js
+++ list.js
@@ -4,16 +4,11 @@ const arr1 = ['a', 'b', 'c', 'd', 'e'];
 const arr2 = arr1.concat('f');
 
 
+2)  Spread Operator
+const arr1 = ['a', 'b', 'c', 'd', 'e'];
 
-
-
-
-
-
-
-
-
-
+const arr2 = [...arr1, 'f']; // ['a', 'b', 'c', 'd', 'e', 'f']
+const arr3 = ['z', ...arr1]; // ['z', 'a', 'b', 'c', 'd', 'e']
 
 
 
